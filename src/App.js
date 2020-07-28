d9d4ac3990f5b28bbf35915c170d07b9b4aa248e
Rajeshkumar <rajeshkumaracet@gmail.com>
2020-07-29 03:58:02 +0530

    Starter file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Login from "./components/login/Login";
+
+const App = () => {
+  return (
+    <div className="root">
+      <Login />
+    </div>
+  );
+};
+
+export default App;
